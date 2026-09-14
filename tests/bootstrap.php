93b891f2dd206adc1d2f5cf871c77324a6d994db
rubenv <ruben.vesterink@zoolt.com>
2026-09-14 10:33:58 +0200

    simplfy bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,32 +2,7 @@
 
 declare(strict_types=1);
 
-namespace {
-    $autoloadPath = dirname(__DIR__) . '/vendor/autoload.php';
-
-    if (is_file($autoloadPath)) {
-        require_once $autoloadPath;
-    }
-
-    foreach (spl_autoload_functions() as $autoloadFunction) {
-        if (
-            is_array($autoloadFunction)
-            && $autoloadFunction[0] instanceof \Composer\Autoload\ClassLoader
-        ) {
-            $autoloadFunction[0]->setPsr4('Enigma\\', [dirname(__DIR__) . '/src']);
-            break;
-        }
-    }
-}
-
 namespace Enigma {
-    class_alias(\DateTimeImmutable::class, __NAMESPACE__ . '\\DateTimeImmutable');
-    class_alias(\DateTimeZone::class, __NAMESPACE__ . '\\DateTimeZone');
-
-    function dd(mixed ...$values): void
-    {
-    }
-
     function curl_init(): \stdClass
     {
         return new \stdClass();
